dbe35cdcf3554102dd7971870f11efdf2b2d3475
LaraCherf <100416018+LaraCherf@users.noreply.github.com>
2022-02-28 10:58:04 +0200

    Update fifth.php

--- fifth.php
+++ fifth.php
@@ -1,8 +1,5 @@
 <?php
-echo '<pre>';
-var_dump($_SERVER["REMOTE_ADDR"]);
-echo '</pre>';
 
 $ip_address = gethostbyname("www.kconsultancy.ae");
-// $ip_address = $_SERVER['HTTP_CLIENT_IP'];
-echo "Google IP address is " . $ip_address;
+
+echo "Website IP address is " . $ip_address;
